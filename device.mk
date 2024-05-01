dde8e6a4fc537a18f43e1527a5cbcb97966bcb19
AntarcticShaurant <109678650+AntarcticShaurant@users.noreply.github.com>
2024-05-01 17:12:00 +0200

    fire: Initial rootdir

--- device.mk
+++ device.mk
@@ -10,6 +10,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt6768 \
+    fstab.mt6768.ramdisk \
+    init.cgroup.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6768.rc \
+    init.mt6768.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    ueventd.mt6768.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
