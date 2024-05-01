a89f6bbfd4e21468a887bf2f1bfa80c7a2d140d0
bengris32 <bengris32@protonmail.ch>
2024-05-01 23:01:21 +0200

    fire: Configure Virtual A/B OTAs
    * Enable recovery-as-boot for A/B device.
    * Build updater packages for A/B device.
    * Include checkpoint_gc & otapreopt_script postinstall
      scripts.
    * Configure partitions which will be included in OTA package.
    * Explicitly disable BOARD_BUILD_SYSTEM_ROOT_IMAGE on A/B device
      using 2-stage-init.
    * Inherit the virtual_ab_ota.mk and compression_retrofit.mk
      product configurations as seen on stock.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,18 @@
 
 DEVICE_PATH := device/xiaomi/fire
 
+# A/B
+AB_OTA_UPDATER := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    product \
+    vendor \
+    vbmeta
 
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
