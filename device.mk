c3a60391b880c851f439dfc9250b1ee0ce2e3297
AntarcticShaurant <109678650+AntarcticShaurant@users.noreply.github.com>
2024-05-01 12:16:40 +0200

    fire: Set PRODUCT_SHIPPING_API_LEVEL to 33

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 32
+PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
