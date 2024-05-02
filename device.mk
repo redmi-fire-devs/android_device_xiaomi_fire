455672dd7421e32507d246c78184c8b12827f9c8
bengris32 <bengris32@protonmail.ch>
2024-05-02 12:27:47 +0200

    fire: Configure Display stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/display/mt6768/proprietary-files.txt
    
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -46,6 +46,15 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.memtrack-service.mediatek-mali \
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@4.0.vendor \
+    libdrm.vendor
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
