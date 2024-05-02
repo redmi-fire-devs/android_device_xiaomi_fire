d69493fc221e834165c9e2ce643e2816d9157499
SamarV-121 <samarvispute121@pm.me>
2024-05-02 22:11:54 +0200

    fire: Link older libutils for PQ HAL
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'

--- device.mk
+++ device.mk
@@ -53,7 +53,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
-    libdrm.vendor
+    libdrm.vendor \
+    libutils-v32
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
