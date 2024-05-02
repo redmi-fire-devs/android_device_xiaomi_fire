2899bc3533df1a6fbb7f143721745dabc24941bc
Bengris32 <bengris32@protonmail.ch>
2024-05-02 08:45:15 +0200

    fire: Include 'hardware/mediatek' Soong namespace
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -72,7 +72,8 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/fire/fire-vendor.mk)
