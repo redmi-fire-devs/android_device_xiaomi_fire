99b95b60da1506454018a37170bec899df80bbca
AntarcticShaurant <109678650+AntarcticShaurant@users.noreply.github.com>
2024-05-08 13:29:48 +0200

    fire: Initial device specific overlays
    based on: https://github.com/mt6768-dev/android_device_xiaomi_earth/commit/19bb310da9de43a115034616cd1fbbfe5033118c

--- device.mk
+++ device.mk
@@ -136,6 +136,14 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayFire \
+    SystemUIOverlayFire \
+    WifiResOverlayFire
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Permissions
