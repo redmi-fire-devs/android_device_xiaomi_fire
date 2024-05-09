954d67ed53f0504acb6554a51b0ed2d787c35b90
surblazer <matvej2002@gmail.com>
2024-05-09 09:26:38 +0200

    fire: Configure Thermal stack

--- device.mk
+++ device.mk
@@ -249,6 +249,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
